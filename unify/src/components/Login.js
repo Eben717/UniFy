5f82444cb3d986f078d66a7cc284822a1a85551f
Eben <kwabenat679@gmail.com>
2025-04-28 23:18:47 +0000

    added login component

---
+++ unify/src/components/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Email:', email);
+        console.log('Password:', password);
+        // Add your login logic here
+    };
+
+    return (
+        <div className="login-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <h2>Login</h2>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter your email"
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter your password"
+                        required
+                    />
+                </div>
+                <button type="submit" className="login-button">Login</button>
+                <p className="signup-link">
+                    Don't have an account? <a href="/signup">Sign up</a>
+                </p>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
